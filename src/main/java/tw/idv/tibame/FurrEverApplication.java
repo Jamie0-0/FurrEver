42d3429745337eeab1a89ff665be405a3d67770b
Jamie <kght1256@gmail.com>
2023-07-21 17:55:11 +0800

    <檔案新增>spring boot環境建置

---
+++ src/main/java/tw/idv/tibame/FurrEverApplication.java
@@ -0,0 +1,13 @@
+package tw.idv.tibame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurrEverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurrEverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/idv/tibame/FurrEverApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.tibame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurrEverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
