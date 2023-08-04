58f2558bf99e24fb845605e2e4603495fc9083a8
TonyYen <bmw266b7540@gmail.com>
2023-08-04 11:49:03 +0800

    <設定>FurEverApplication更新

--- src/main/java/tw/idv/tibame/FurrEverApplication.java
+++ src/main/java/tw/idv/tibame/FurrEverApplication.java
@@ -2,8 +2,14 @@ package tw.idv.tibame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ComponentScan("tw.idv.tibame.*.*")
+@EnableJpaRepositories("tw.idv.tibame.articles.dao")
+@EnableTransactionManagement
 public class FurrEverApplication {
 
 	public static void main(String[] args) {
