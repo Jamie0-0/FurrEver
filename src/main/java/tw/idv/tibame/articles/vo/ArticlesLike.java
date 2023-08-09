9393388ca7ea01f0b73ba0c9947e5570c0a004c1
TonyYen <bmw266b7540@gmail.com>
2023-08-09 19:51:31 +0800

    整理程式碼

--- src/main/java/tw/idv/tibame/articles/vo/ArticlesLike.java
+++ src/main/java/tw/idv/tibame/articles/vo/ArticlesLike.java
@@ -3,10 +3,7 @@ package tw.idv.tibame.articles.vo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
